d439823afbd9e625a77b7fffa6bed23865d34ec0
Priscilla Alexis Mitchell <alexismitchell42@gmail.com>
2023-02-16 14:30:26 -0600

    Updated index.html_. js_main.css

---
+++ myScript.js
@@ -0,0 +1,41 @@
+//This is a loop!
+const names = ["Ed", "John", "Maria", "Eliza", "Burrito", "Harry", "Potter"];
+
+for (name of names){
+    console.log(`Hello there ${name}`);
+}
+
+//This is a while loop
+
+let loading = 0;
+
+while (loading < 100){
+    console.log("Website is still loading");
+loading++;
+}
+
+//Change css with js_DOM element
+//Change css based upon variable value
+const text = document.querySelector(".title");
+const changeColor = document.querySelector(".changeColor");
+
+changeColor.addEventListener('click', function(){
+    text.classList.toggle("change"); 
+} )
+
+
+//Changes font and color of main header
+{
+const main = document.querySelector('.main');
+text.classList.add =("main");
+}
+
+
+const typo = document.querySelector (".typography")
+const push = document.querySelector(''.changeText')
+
+changeText.addEventListener('click', function(){
+    text.classList.toggle("typo");  
+} )
+
+
